d72275ce90f0741ae5a8245195a19b0912b29c66
shoxrux0107 <147584632+shoxrux0107@users.noreply.github.com>
2025-04-22 23:03:35 +0500

    Update DemoApplicationTests.java fro unnoticed error

--- backend/demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ backend/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,12 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
-class DemoApplicationTests {
+@SpringBootApplication
+public class RunMyRouteBackendApplication {
 
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.run(RunMyRouteBackendApplication.class, args);
 	}
-
 }
